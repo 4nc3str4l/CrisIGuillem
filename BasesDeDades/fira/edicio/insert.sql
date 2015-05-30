fca85b6ffa75ed57ffe8356e2515005ca729cacc
Blipi <gpascualg93@gmail.com>
2015-05-30 13:30:27 +0200

    Fix anual / bianual

--- BasesDeDades/fira/edicio/insert.sql
+++ BasesDeDades/fira/edicio/insert.sql
@@ -1,5 +1,5 @@
 \echo ---------- insert edicio
-INSERT INTO edicio VALUES ('2005/1/23', '2005/2/23', 0, 0, 'Esdeveniment 2'),
-('2003/8/8', '2003/9/8', 0, 0, 'Esdeveniment 2'),
-('2013/10/18', '2013/12/18', 0, 0, 'Esdeveniment 2'),
-('2011/6/22', '2011/7/22', 0, 0, 'Esdeveniment 2');
+INSERT INTO edicio VALUES ('2001/9/14', '2001/11/14', 0, 0, 'Esdeveniment 1'),
+('2003/9/14', '2003/11/14', 0, 0, 'Esdeveniment 1'),
+('2005/9/14', '2005/10/14', 0, 0, 'Esdeveniment 1'),
+('2013/10/18', '2013/12/18', 0, 0, 'Esdeveniment 2');
--- BasesDeDades/fira/estand/insert.sql
+++ BasesDeDades/fira/estand/insert.sql
@@ -1,11 +1,11 @@
 \echo ---------- insert estand
-INSERT INTO estand VALUES ('Estand 1', true, 386, 46, 38, 'Ubicacio 3'),
-('Estand 2', true, 1189, 47, 20, 'Ubicacio 2'),
-('Estand 3', true, 669, 26, 34, 'Ubicacio 4'),
-('Estand 4', true, 961, 33, 26, 'Ubicacio 4'),
-('Estand 5', true, 589, 21, 44, 'Ubicacio 2'),
-('Estand 6', false, 597, 26, 50, 'Ubicacio 1'),
-('Estand 7', true, 567, 32, 33, 'Ubicacio 2'),
-('Estand 8', true, 838, 33, 22, 'Ubicacio 2'),
-('Estand 9', true, 455, 23, 25, 'Ubicacio 3'),
-('Estand 10', false, 779, 33, 28, 'Ubicacio 4');
+INSERT INTO estand VALUES ('Estand 1', true, 778, 43, 33, 'Ubicacio 3'),
+('Estand 2', true, 973, 22, 46, 'Ubicacio 3'),
+('Estand 3', false, 1091, 50, 47, 'Ubicacio 3'),
+('Estand 4', true, 957, 32, 26, 'Ubicacio 1'),
+('Estand 5', true, 926, 42, 33, 'Ubicacio 2'),
+('Estand 6', false, 1027, 29, 21, 'Ubicacio 1'),
+('Estand 7', false, 360, 30, 26, 'Ubicacio 4'),
+('Estand 8', false, 850, 29, 32, 'Ubicacio 4'),
+('Estand 9', false, 1059, 38, 33, 'Ubicacio 2'),
+('Estand 10', true, 964, 25, 23, 'Ubicacio 1');
